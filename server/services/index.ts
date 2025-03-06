062280396534c5fad7f563fb396f793340c850e8
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2025-03-06 13:32:46 +0000

    BTD-546: Prisoner Search Page (#5)
    * Work in progress
    
    * New client
    
    * Cleanup
    
    * Working
    
    * Cleanups
    
    * cleanup
    
    * linting
    
    * style
    
    * audit
    
    * Refactor tests
    
    * Refactor to have simpler model
    
    * Refactor to have simpler model
    
    * remove template literal
    
    * use gov components
    
    * remove useless css class
    
    * redirect fix
    
    ---------
    
    Co-authored-by: Ewan Donovan <ewan.donovan@digital.justice.co.uk>

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,19 @@
 import { dataAccess } from '../data'
+import PrisonerSearchClient from '../data/prisonerSearch/prisonerSearchClient'
 import AuditService from './auditService'
+import PrisonerSearchService from './prisonerSearch/prisonerSearchService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, hmppsAuthClient } = dataAccess()
 
   const auditService = new AuditService(hmppsAuditClient)
+
+  const prisonerSearchService = new PrisonerSearchService(hmppsAuthClient, new PrisonerSearchClient())
 
   return {
     applicationInfo,
     auditService,
+    prisonerSearchService,
   }
 }
 
