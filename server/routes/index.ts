c16e43622a37af9c105a41c45b336570aa2c37ea
Patrick Lucas <patrick.lucas@justice.gov.uk>
2025-03-07 12:15:34 +0000

    [BTD-598] More changes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -10,7 +10,7 @@ export default function routes(services: Services): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
-    services.auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
+    services.auditService.logPageView(Page.LANDING_PAGE, { who: res.locals.user.username, correlationId: req.id })
     res.render('pages/index')
   })
 
