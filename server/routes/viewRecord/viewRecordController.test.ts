c82c9d93a2b766e32d0899798217969812a32971
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-03-12 16:29:32 +0000

    import fix

--- server/routes/viewRecord/viewRecordController.test.ts
+++ server/routes/viewRecord/viewRecordController.test.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from 'express'
+import { Request, Response } from 'express'
 import ViewRecordController from './viewRecordController'
 import AuditService, { Page } from '../../services/auditService'
 import PrisonerSearchService from '../../services/prisonerSearch/prisonerSearchService'
