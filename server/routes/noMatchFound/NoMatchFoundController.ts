9f547bf664ea1c5f0dcc2a3344dc5f74dfdcfb38
Malawarayan Ganesan <malawarayan.ganesan@digital.justice.gov.uk>
2025-03-13 10:37:25 +0000

    feat(*): address review comments

--- server/routes/noMatchFound/NoMatchFoundController.ts
+++ server/routes/noMatchFound/NoMatchFoundController.ts
@@ -4,8 +4,7 @@ export default class NoMatchFoundController {
   constructor() {}
 
   getNoMatchFound: RequestHandler = async (req, res, next): Promise<void> => {
-    const { givenName } = req.session.searchByInformationForm
-    const { familyName } = req.session.searchByInformationForm
+    const { givenName, familyName } = req.session.searchByInformationForm
     const { prisonerNumber } = req.params
     return res.render('pages/noMatchFound', {
       givenName,
