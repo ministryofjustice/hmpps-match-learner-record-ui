b973134f61e5b276e86e1ccd11d446403785835a
Patrick Lucas <patrick.lucas@justice.gov.uk>
2025-03-18 11:45:17 +0000

    [BTD-634] Format dates

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -21,3 +21,20 @@ export const initialiseName = (fullName?: string): string | null => {
   const array = fullName.split(' ')
   return `${array[0][0]}. ${array.reverse()[0]}`
 }
+
+/**
+ * Formats an ISO8601 datetime string into the format specified by the GOV.UK Style guide
+ * e.g. 1 January 2022
+ * https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style#dates
+ * @param datestring A datetime string e.g. 2011-10-05T14:48:00.000Z or 2001-06-03
+ * @returns A string
+ * */
+export function govukFormattedFullDateString(datestring?: string): string {
+  const date = datestring ? new Date(datestring) : new Date()
+
+  return new Date(date).toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+}
