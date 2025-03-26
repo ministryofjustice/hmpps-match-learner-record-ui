077510a31614e829ca3e0554ea399a198541bd2e
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-03-26 15:46:14 +0000

    Removed unused import

--- integration_tests/e2e/signIn.cy.ts
+++ integration_tests/e2e/signIn.cy.ts
@@ -1,7 +1,6 @@
 import IndexPage from '../pages/index'
 import AuthSignInPage from '../pages/authSignIn'
 import Page from '../pages/page'
-import AuthManageDetailsPage from '../pages/authManageDetails'
 
 context('Sign In', () => {
   beforeEach(() => {
