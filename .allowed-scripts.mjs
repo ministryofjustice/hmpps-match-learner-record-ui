5032c4e9d01a8efeba160905808ccaa107685f0c
Mary Ford <mary.ford@justice.gov.uk>
2025-12-11 15:31:12 +0000

    Fix linting

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -1,11 +1,11 @@
 import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
 
 export default configureAllowedScripts({
-   allowlist: {
-     "node_modules/@parcel/watcher@2.5.1": "ALLOW",
-     "node_modules/cypress@14.0.3": "ALLOW",
-     "node_modules/dtrace-provider@0.8.8": "ALLOW",
-     "node_modules/esbuild@0.25.9": "ALLOW",
-     "node_modules/fsevents@2.3.3": "ALLOW"
-   },
+  allowlist: {
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/cypress@14.0.3': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/esbuild@0.25.9': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'ALLOW',
+  },
 })
